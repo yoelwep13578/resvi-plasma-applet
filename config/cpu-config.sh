f5df5c6653fd7321c4858a11e9f6e4def3b60d89
yoelwep13578 <wiyadiekaputrayoel@gmail.com>
2025-02-05 16:54:53 +0700

    ResVi CPU Config - Attempt 2

--- config/cpu-config.sh
+++ config/cpu-config.sh
@@ -5,10 +5,18 @@ display_as="label,load,bar,max"
 
 # LABEL
 label_type="icon"
-label_icon=""
+label_icon=""  # Ikon default
 label_text="CPU"
 label_left_sign="["
 label_right_sign="]"
+
+# Dynamic icon configuration
+label_dynamic_icon="no"
+label_icon_level1="0, "
+label_icon_level2="40,󰾆"
+label_icon_level3="70,󰾅"
+label_icon_level4="101,󰓅"
+label_icon_level5="101, "
 
 # LOAD/CURRENT
 loadinfo_fixed_width="4"
@@ -19,14 +27,14 @@ loadinfo_left_sign="["
 loadinfo_right_sign="]"
 
 # BAR
-bar_length="medium"        # Bisa diisi "shortest", "short", "medium", "long", "longest", atau angka
+bar_length="medium"
 bar_fill_character="/"
-bar_empty_character="."
+bar_empty_character="_"
 bar_left_sign=" "
 bar_right_sign=" "
 
 # MAX/CAPACITY
-maxcap_fixed_width="no"
+maxcap_fixed_width="4"
 maxcap_text_align="left"
 maxcap_type="text"
 maxcap_text="100%"
